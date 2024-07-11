9db392c45e8edc1392653e8b29b355e1648cf9f9
Rupert Hudson <141234410+ruperthudson@users.noreply.github.com>
2024-07-11 09:59:18 +0000

    Caught empty json exception.

--- submit2.php
+++ submit2.php
@@ -6,15 +6,24 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Exception\JsonException;
 
 function processRequest(Request $req): Response {
-    $data = $req -> toArray();
+    try {
+        $data = $req -> toArray();
+    } catch(JsonException $e) {
+        return new Response(
+            "Bad request: request is malformed.",
+            Response::HTTP_BAD_REQUEST,
+            ['content-type' => 'text/plain'],
+        );
+    }
 
     $firstname = $data["firstname"];
     
     $secondname = $data["secondname"];
     
-    if (strlen($firstname) == 0) {
+    if (strlen($firstname) == 0 or strlen($secondname) == 0) {
         return new Response(
             "Bad request: required form inputs should not be empty.",
             Response::HTTP_BAD_REQUEST,
