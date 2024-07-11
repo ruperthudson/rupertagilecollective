b92f13eaf0ca91ac94189a64b31e6465d450b38d
Rupert Hudson <141234410+ruperthudson@users.noreply.github.com>
2024-07-11 09:41:32 +0000

    Refactor to use Symfony. Added input validation.

--- submit2.php
+++ submit2.php
@@ -1,8 +1,40 @@
 <?php
-$jsonData = file_get_contents('php://input');
-$data = json_decode($jsonData, true);
-$firstname = $data["firstname"];
-$secondname = $data["secondname"];
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($firstname . " " . $secondname);
+
+declare(strict_types = 1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+function processRequest(Request $req): Response {
+    $data = $req -> toArray();
+
+    $firstname = $data["firstname"];
+    
+    $secondname = $data["secondname"];
+    
+    if (strlen($firstname) == 0) {
+        return new Response(
+            "Bad request: required form inputs should not be empty.",
+            Response::HTTP_BAD_REQUEST,
+            ['content-type' => 'text/plain'],
+        );
+    }
+    
+    return new Response(
+        json_encode($firstname . " " . $secondname),
+        Response::HTTP_OK,
+        ['content-type' => 'application/json'],
+    );
+}
+
+// $req = new Request([], [], [], [], [], [], '{"firstname":"", "secondname":"bar"}');
+
+$req = Request::createFromGlobals();
+
+$resp = processRequest($req);
+
+$resp -> send();
+
 ?>
